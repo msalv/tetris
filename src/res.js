f69c09a4f4172ae68e0a5e22e6e701831525811f
Kirill Bykov <bikovgta13@gmail.com>
2016-07-24 23:59:20 +0300

    Add gray color

--- src/res.js
+++ src/res.js
@@ -1,10 +1,11 @@
 export const colors = {
-	RED: "#F44336",
-	BLUE: "#2196F3",
-	GREEN: "#8BC34A",
+	RED:    "#F44336",
+	BLUE:   "#2196F3",
+	GREEN:  "#8BC34A",
 	YELLOW: "#FFC107",
 	PURPLE: "#9C27B0",
-	BLACK: "#000000"
+	BLACK:  "#000000",
+	GRAY:   "#212121"
 };
 
 export const dimen = {
